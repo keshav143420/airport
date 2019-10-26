4257ce2ced1caa19c686702614a3ec056671485b
keshava kotri <keshav143420@gmail.com>
2019-10-26 19:04:06 +0530

    added unit testcase

---
+++ src/test/java/com/kk/tdd/airport/AirportTest.java
@@ -0,0 +1,28 @@
+package com.kk.tdd.airport;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AirportTest {
+
+    @Test
+    public void testEconomyFlightUsualPassenger() {
+        Flight economyFlight = new Flight("1", "Economy");
+        Flight businessFlight = new Flight("2", "Business");
+
+        Passenger john = new Passenger("John", true);
+        Passenger mike = new Passenger("Mike", false);
+
+        assertEquals(true, economyFlight.addPassenger(john));
+        assertEquals(false, economyFlight.removePassenger(john));
+        assertEquals(true, businessFlight.addPassenger(john));
+        assertEquals(false, businessFlight.removePassenger(john));
+
+        assertEquals(true, economyFlight.addPassenger(mike));
+        assertEquals(true, economyFlight.removePassenger(mike));
+        assertEquals(false, businessFlight.addPassenger(mike));
+        assertEquals(false, businessFlight.removePassenger(mike));
+    }
+
+}
